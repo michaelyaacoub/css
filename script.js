a0c5a58df9ba0a3f40f7c715531f7ed89203ca26
Michael Yaacoub <michael.yaacoub7@gmail.com>
2023-01-12 22:03:59 -0800

    added a DOMContentLoaded f()

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-document.querySelector('#btn-post').addEventListener('click', function () {
-    if ('click') {
-        document.querySelector('#btn-post').innerHTML = "This feature is coming soon! 🙃";
-    }
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('#btn-post').addEventListener('click', function () {
+        if ('click') {
+            document.querySelector('#btn-post').innerHTML = "This feature is coming soon! 🙃";
+        }
+    });
 });
