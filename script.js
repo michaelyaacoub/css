20900c08eb58e4aa276f726e47203ebd96538b42
Michael Yaacoub <michael.yaacoub7@gmail.com>
2023-01-24 20:46:52 -0800

    form completed

--- script.js
+++ script.js
@@ -1,10 +1,48 @@
 // load html first then perform events
 document.addEventListener('DOMContentLoaded', function () {
-    document.querySelector('#btn-post').addEventListener('click', function () {
+    document.querySelector('#temp').addEventListener('click', function () {
         if ('click') {
-            document.querySelector('#btn-post').innerHTML = "This feature is coming soon! 🙃";
-            document.querySelector('#btn-post').style.color = 'blue';
+            document.querySelector('#temp').innerHTML = "This feature is coming soon! 🙃";
+            document.querySelector('#temp').style.color = 'blue';
         }
     });
 });
 
+// Form loading buttons
+// querySelectorAll might cause a bug when submitting the form later
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
